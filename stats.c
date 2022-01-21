b225947606fe9930390dc8d46b4885c5bc1380d6
meshalalhazmi <meshal.alhazmi@gmail.com>
2022-01-21 22:04:28 +0300

    update .h and .c  documentation

--- stats.c
+++ stats.c
@@ -39,8 +39,15 @@ void main()
   /* Statistics and Printing Functions Go Here */
 }
 
-/*
+/**
+ * @brief
+ *
  * A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return void
  */
 void print_statistics(unsigned char array[], unsigned int array_size)
 {
@@ -53,9 +60,16 @@ void print_statistics(unsigned char array[], unsigned int array_size)
   printf("sorted array is: \n");
   print_array(array, array_size);
 }
-/*
+
+/**
+ * @brief <Add Brief Description of Function Here>
  *
  * Given an array of data and a length, prints the array to the screen
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return void
  */
 void print_array(unsigned char array[], unsigned int array_size)
 {
@@ -64,9 +78,16 @@ void print_array(unsigned char array[], unsigned int array_size)
     printf("array[%d] : %d \n", i, array[i]);
   }
 }
-/*
+
+/**
+ * @brief <Add Brief Description of Function Here>
  *
  * Given an array of data and a length, returns the median value
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return unsigned char of the median value in the supplied array
  */
 unsigned char find_median(unsigned char array[], unsigned int array_size)
 {
@@ -91,9 +112,15 @@ unsigned char find_median(unsigned char array[], unsigned int array_size)
     return 0;
   }
 }
-/*
+/**
+ * @brief <Add Brief Description of Function Here>
  *
  * Given an array of data and a length, returns the mean
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return unsigned char of the mean value of the supplied array
  */
 unsigned char find_mean(unsigned char array[], unsigned int array_size)
 {
@@ -105,9 +132,15 @@ unsigned char find_mean(unsigned char array[], unsigned int array_size)
   }
   return (mean / array_size);
 }
-/*
+/**
+ * @brief <Add Brief Description of Function Here>
  *
  * Given an array of data and a length, returns the maximum
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return unsigned char of the maximum value in the supplied array
  */
 unsigned char find_maximum(unsigned char array[], unsigned int array_size)
 {
@@ -119,9 +152,15 @@ unsigned char find_maximum(unsigned char array[], unsigned int array_size)
   }
   return array[maximum];
 }
-/*
+/**
+ * @brief <Add Brief Description of Function Here>
  *
  * Given an array of data and a length, returns the minimum
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return unsigned char of the minimum value in the supplied array
  */
 unsigned char find_minimum(unsigned char array[], unsigned int array_size)
 {
@@ -133,11 +172,17 @@ unsigned char find_minimum(unsigned char array[], unsigned int array_size)
   }
   return array[minimum];
 }
-/*
-*
-* Given an array of data and a length, sorts the array from largest to smallest.
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * Given an array of data and a length, sorts the array from largest to smallest.
  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
-*/
+ *
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
+ *
+ * @return void
+ */
 void sort_array(unsigned char array[], unsigned int array_size)
 {
 
--- stats.h
+++ stats.h
@@ -1,15 +1,15 @@
 /******************************************************************************
- * Copyright (C) 2022 by Meshal Alhazmi 
+ * Copyright (C) 2022 by Meshal Alhazmi
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Meshal Alhazmi is not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
+ * @file <Add File Name>
  * @brief <Add Brief Description Here >
  *
  * This is a c sample code to analyze an array of unsigned char data items and report analytics on the maximum, minimum, mean, and median of the data set.
@@ -22,7 +22,7 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Add Your Declarations and Function Comments here */
 
 /**
  * @brief
@@ -32,7 +32,7 @@
  * @param arry An unsigned char pointer to an n-element data array
  * @param array_size  An unsigned integer as the size of the array
  *
- * @return An unsigned char result from the function
+ * @return void
  */
 void print_statistics(unsigned char array[], unsigned int array_size);
 /**
@@ -40,12 +40,10 @@ void print_statistics(unsigned char array[], unsigned int array_size);
  *
  * Given an array of data and a length, prints the array to the screen
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
 void print_array(unsigned char array[], unsigned int array_size);
 /**
@@ -53,12 +51,10 @@ void print_array(unsigned char array[], unsigned int array_size);
  *
  * Given an array of data and a length, returns the median value
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return unsigned char of the median value in the supplied array
  */
 unsigned char find_median(unsigned char array[], unsigned int array_size);
 /**
@@ -66,12 +62,10 @@ unsigned char find_median(unsigned char array[], unsigned int array_size);
  *
  * Given an array of data and a length, returns the mean
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return unsigned char of the mean value of the supplied array
  */
 unsigned char find_mean(unsigned char array[], unsigned int array_size);
 /**
@@ -79,39 +73,33 @@ unsigned char find_mean(unsigned char array[], unsigned int array_size);
  *
  * Given an array of data and a length, returns the maximum
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return unsigned char of the maximum value in the supplied array
  */
-unsigned char find_maximum(unsigned char array[], unsigned int array_size) ;
+unsigned char find_maximum(unsigned char array[], unsigned int array_size);
 /**
  * @brief <Add Brief Description of Function Here>
  *
  * Given an array of data and a length, returns the minimum
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return unsigned char of the minimum value in the supplied array
  */
-unsigned char find_minimum(unsigned char array[], unsigned int array_size); 
+unsigned char find_minimum(unsigned char array[], unsigned int array_size);
 /**
  * @brief <Add Brief Description of Function Here>
  *
- * Given an array of data and a length, sorts the array from largest to smallest. 
+ * Given an array of data and a length, sorts the array from largest to smallest.
  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param arry An unsigned char pointer to an n-element data array
+ * @param array_size  An unsigned integer as the size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
 void sort_array(unsigned char array[], unsigned int array_size);
 
